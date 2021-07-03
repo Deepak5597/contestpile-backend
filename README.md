1e7d31ca019255e2582475979d8cf28f24d1ac63
technozent <32607176+Deepak5597@users.noreply.github.com>
2021-07-03 17:22:55 +0530

    First commit for contestpile backend

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# contestpile-backend
+# Strapi application
+
+A quick description of your strapi application
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '716026e1-92b4-4a61-9496-b2c1e247e6da'
+};
